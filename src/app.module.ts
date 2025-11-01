6fead23538d119236908f7f4d603ebd3a3c1257d
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2025-11-02 02:24:26 +0500

    Remove AppController and AppService along with their tests; add global ValidationPipe in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe(),
+    },
+  ],
 })
 export class AppModule {}
