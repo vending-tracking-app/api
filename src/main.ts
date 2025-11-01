3522def92f47899ca582900c6c0a6658d9135c0d
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2025-11-02 02:28:12 +0500

    Add class-transformer and class-validator dependencies; refactor main.ts to retrieve PORT from ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = parseInt(configService.getOrThrow('PORT'));
+  await app.listen(port);
 }
 
 void bootstrap();
