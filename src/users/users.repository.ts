d4bc740c08afcb28a58707251f6836d2a410e8f8
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2025-12-01 14:57:00 +0500

    Add documentation comment in UsersRepository to clarify its purpose for user retrieval only, directing other operations to AuthService.

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -5,6 +5,10 @@ import { TransactionalAdapterTypeOrm } from '@nestjs-cls/transactional-adapter-t
 import { BaseRepository } from '../db/base.repository';
 import { User } from './entities/user.entity';
 
+/**
+ * This repository should be used only to retrieve users from the database.
+ * For other operations, use the AuthService.
+ */
 @Injectable()
 export class UsersRepository extends BaseRepository<User> {
   constructor(txHost: TransactionHost<TransactionalAdapterTypeOrm>) {
