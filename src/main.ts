017f05dfe0f336c62217f9863a4d2fbc6596c5cb
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2026-01-25 19:48:02 +0500

    Update path to swagger

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   
   const config = new DocumentBuilder().build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory);
   
   const configService = app.get(ConfigService);
   const port = configService.get('env.port');
